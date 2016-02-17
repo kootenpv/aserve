ceed47ec61a1e954ce08fbb5e551d5264964978a
Pascal van Kooten <kootenpv@gmail.com>
2016-02-17 20:34:50 +0100

    updated readme and added argument parser

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-## mocka
+## mocka (pre-alpha)
 
-Mocking APIs with mocka!
+The goal of mocka is to make it easy to "mock an API". If you want to
+serve some text, json, or echo a request: mocka enables quick
+testing.
+
+- Hackathon: give your buddy an endpoint with a statically served JSON
+- Server-to-server: viewing how a request looks like
+- Send your custom response: use `pdb` to drop in a request and return a response manually
+
+### Features
+
+- Easy to use, just a command line required ;)
+- Asynchronous
+- Serve different types of data
+- Automatically supports GET/POST/HEAD/OPTIONS
+- Pipe data to mocka to serve it: `cat some.json | mocka`
+
+### Installation
+
+Only works on Python 3.5+ at the moment.
+
+    pip3.5 install mocka
+
+### How to:
+
+```
+usage: mocka [-h] [--debug] [--port PORT] [--verbose] [--file FILE] [--sleep SLEEP]
+
+Mock an API with mocka.
+
+optional arguments:
+  -h, --help                   show this help message and exit
+  --debug, -d                  Uses "pdb" to drop you into the request, BEFORE replying
+  --port PORT, -p PORT         Port where to host
+  --verbose, -v                Talks.... a lot.
+  --file FILE, -f FILE         Loads file (COMING VERY SOON)
+  --sleep SLEEP, -s SLEEP      seconds to wait before delivering
+```
+
+### Looking for direction
+
+I will most likely completely rewrite this library at some point, just trying to get some ideas for now.
+Perhaps helping with speedtests or something. Perhaps to enable easy of setting up an API.
+
+### Caveats
+
+Currently, it uses `aiohttp` and asyncio, and mocka is only available from Python 3.5+. It should be possible in the future to use a different backend.
+Reasons for a different backend might be to not require any other installation, or perhaps to support
--- mocka/mocka.py
+++ mocka/mocka.py
@@ -1,32 +1,30 @@
 from aiohttp import web
 import json
 
+import asyncio
 import sys
 import os
 from stat import S_ISFIFO
 
-piped_content = sys.stdin.read() if S_ISFIFO(os.fstat(0).st_mode) else ''
-
-# first position or stdin: content
-# serving content as json dump
-# -f --file: serve content as file
-# -d --debugger: use pdb
-# -v --verbose: print all
-
 
 def ensure_bytes(x):
+    """ Ensure 'x' is going to be served as bytes. """
     if not isinstance(x, bytes):
         x = bytes(str(x).encode("utf8"))
     return x
 
 
 def echo(request):
+    """ The response will reflect what the request looks like. """
     dc = getattr(request, "__dict__")
     return json.dumps({k: str(v) for k, v in dc.items()})
 
 
-def route_wrapper(reply, use_pdb=False, printing=False):
+def route_wrapper(reply, use_pdb=False, printing=False, sleepfor=0):
+    """ Wrapper to easily create routes. """
     async def route_function(request):
+        if sleepfor:
+            await asyncio.sleep(sleepfor)
         if use_pdb:
             import pdb
             pdb.set_trace()
@@ -45,21 +43,31 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Mock an API with mocka.')
     parser.add_argument('--debug', '-d', action="store_true",
-                        help='Uses "pdb" to drop you into the request, before replying')
+                        help='Uses "pdb" to drop you into the request, BEFORE replying')
     parser.add_argument('--port', '-p', type=int, default=8080,
                         help='Port where to host')
     parser.add_argument('--verbose', '-v', action="store_true",
                         help='Talks.... a lot.')
     parser.add_argument('--file', '-f', nargs=1,
-                        help='Loads file')
+                        help='Loads file (COMING VERY SOON)')
+    parser.add_argument('--sleep', '-s', type=int, default=0,
+                        help='seconds to wait before delivering')
     args = parser.parse_args()
 
     args.file = args.file[0] if args.file else None
+    if args.verbose:
+        print("Going to talk a lot.")
+        print("Debugging mode {}.".format(args.debug))
+        if args.sleep:
+            print("Sleeping between requests: {}s.".format(args.sleep))
+        if args.file is not None:
+            raise NotImplementedError("Planned to come soon. Check back later."):
+            print("serving file at /file")
     return args
 
 
 def main():
-    """ This is the function that is run from commandline with `yagmail` """
+    """ This is the function that is run from commandline with `mocka` """
     app = web.Application()
 
     args = parse_args()
@@ -76,6 +84,8 @@ def main():
         with open(args.file) as f:
             RESPONSES['file'] = json.dumps(f.read())
 
+    piped_content = sys.stdin.read() if S_ISFIFO(os.fstat(0).st_mode) else ''
+
     if piped_content:
         try:
             RESPONSES['json'] = json.dumps(json.loads(piped_content))
@@ -86,14 +96,15 @@ def main():
     ROUTER = {}
     for m in METHODS:
         for d in DTYPES:
-            ROUTER[(m.lower(), d)] = route_wrapper(RESPONSES[d], args.debug, args.verbose)
+            route = route_wrapper(RESPONSES[d], args.debug, args.verbose, args.sleep)
+            ROUTER[(m.lower(), d)] = route
 
     for r, route_fn in ROUTER.items():
         app.router.add_route(r[0], '/{}_{}'.format(*r), route_fn)
 
     for m in METHODS:
         route_fn = ensure_bytes(json.dumps(sorted(['/' + '_'.join(x) for x in ROUTER])))
-        app.router.add_route(m, '/', route_wrapper(route_fn))
+        app.router.add_route(m, '/', route_wrapper(route_fn, args.debug, args.verbose, args.sleep))
 
     web.run_app(app, port=args.port)
 
